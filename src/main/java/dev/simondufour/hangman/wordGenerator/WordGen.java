10139034c22329c3af868f4c9e13b4dd3474f1fe
Simon Dufour <sim.dufour@gmail.com>
2021-07-25 22:40:06 -0400

    WordGenerator and basic UI
    - The UI was used to test the WordGenerator.
    - WordGenerator should be added to Game class using dependency injection.

---
+++ src/main/java/dev/simondufour/hangman/wordGenerator/WordGen.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dev.simondufour.hangman.wordGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ *
+ * @author Simon Dufour
+ */
+public class WordGen {
+    private Random r = new Random();
+    private ArrayList<String> backupWordList = new ArrayList<String>();
+    private ArrayList<String> wordList = new ArrayList<String>();
+
+    public WordGen() {
+        addBuiltInWords();
+    }
+    
+    public WordGen(String[] specificWordList, boolean alsoUseBuiltWords) {
+        if(alsoUseBuiltWords) {
+            addBuiltInWords();
+        }
+        
+        wordList.addAll(List.of(specificWordList));
+        
+        backupWordList.addAll(wordList);
+    }
+    
+    public WordGen(String url) {
+        throw new java.lang.UnsupportedOperationException("Not implemented yet");
+    }
+    
+    private void addBuiltInWords() {
+        wordList.add("PATATE");
+        wordList.add("POMME");
+        wordList.add("OREILLE");
+        wordList.add("TRAVAUX");
+        wordList.add("APPRENDRE");
+        wordList.add("RECTANGLE");
+        wordList.add("MAISON");
+        wordList.add("VOITURE");
+        
+        backupWordList.addAll(wordList);
+    }
+    
+    public int count() {
+        return wordList.size();
+    }
+    
+    public int totalCount() {
+        return backupWordList.size();
+    }
+    
+    public boolean removeWord(String word, boolean removeFromWordlist) {
+        if(removeFromWordlist) {
+            backupWordList.remove(word);
+        }
+        
+        return wordList.remove(word);
+    }
+    
+    public boolean addWord(String word) {
+        backupWordList.add(word);
+        return wordList.add(word);
+    }
+    
+    public void resetWordList() {
+        wordList = (ArrayList<String>)backupWordList.clone();
+    }
+    
+    public String getRandomWord() {
+        if(count() == 0) {
+            System.out.println("No more words left. Resetting.");
+            resetWordList();
+        }
+        
+        int randomIndex = r.nextInt(count());
+        String result = wordList.get(randomIndex);
+        removeWord(result, false);
+        System.out.println("Random Word: " + result);
+        return result;
+    }
+}
